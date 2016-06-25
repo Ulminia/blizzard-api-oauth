2fc48f89f02d62dea23f548d9e1fd6e01a56e56e
Ulminia <ulminia@gmail.com>
2016-06-25 15:53:28 -0300

    index update

--- index.php
+++ index.php
@@ -26,24 +26,21 @@ $redirect_uri		= '';
 $client = new OAuth2\Client($client_id, $client_secret, $region, $locale, $redirect_uri);
 
 
-$r = $client->fetch('character',array('name'=>'ulminia','server'=>'zangarmarsh','fields'=>'items,stats','source'=>'wow'));
-echo '<pre>';
-print_r($r);
-echo '</pre>';
-
 if (!isset($_GET['code']))
 {
-
 	$auth_url = $client->getAuthenticationUrl($client->baseurl[$client->region]['AUTHORIZATION_ENDPOINT'], $client->redirect_uri);
+	//echo $auth_url;
 	header('Location: ' . $auth_url);
-	die('Redirect');
 }
 else
 {
 	$params = array('code' => $_GET['code'], 'auth_flow' => 'auth_code', 'redirect_uri' => $client->redirect_uri);
 	$response = $client->getAccessToken($client->baseurl[$client->region]['TOKEN_ENDPOINT'], 'authorization_code', $params);
+echo '<pre>';
+print_r($response);
+echo '</pre>';
 	$client->setAccessToken($response['result']['access_token']);
-	$response = $client->fetch('wowprofile');
+	$response = $client->fetch('user',array('source'=>'account'));
 	echo '<pre>';
 	print_r($response);
 	echo '</pre>';
